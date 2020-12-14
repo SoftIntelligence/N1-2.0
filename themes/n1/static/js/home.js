7eb60b6c26c5889a79d05b227bb975d3f73115f7
root <root@localhost.localdomain>
2020-12-14 17:25:55 +0630

    N1

--- themes/n1/static/js/home.js
+++ themes/n1/static/js/home.js
@@ -17,39 +17,9 @@ function showcat(){
 	
 }
 
-const Kdrama = "https://creator.n1channel.org/drama/search.php/?s=KDrama";
-const Jdrama = "https://creator.n1channel.org/drama/search.php/?s=JapaneseDrama";
-const ChineseDrama = "https://creator.n1channel.org/drama/search.php/?s=ChineseDrama";
-const Anime = "https://creator.n1channel.org/drama/search.php/?s=Anime";
 
 const ad = "https://creator.n1channel.org/ad/read.php";
-
-
-
-getapi(Kdrama,Jdrama,ChineseDrama,Anime);
 getapiad(ad); 
-
-
-
-async function getapi(url,url2,url3,url4) { 
-  
-  // Storing response 
-  const response = await fetch(url); 
-  const response2 = await fetch(url2);
-  const response3= await fetch(url3);
-  const response4 = await fetch(url4);   
-  
-  // Storing data in form of JSON 
-  var data = await response.json();
-  var data2 = await response2.json(); 
-  var data3 = await response3.json();
-  var data4 = await response4.json();
-  console.log(data); 
-  if (response) { 
-     
-  } 
-  showKdrama(data,data2.data3,data4); 
-} 
 async function getapiad(url) { 
   
   // Storing response 
@@ -74,7 +44,28 @@ document.getElementById("hpad2").src = r.url;
 }	
 }	
 }
-function showKdrama(data,data2,data3,data4){
+
+
+
+const Kdrama = "https://creator.n1channel.org/drama/search.php/?s=Kdrama";
+getapi(Kdrama);
+async function getapi(url) { 
+  
+  // Storing response 
+  const response = await fetch(url); 
+  
+  
+  // Storing data in form of JSON 
+  var data = await response.json();
+  
+  console.log(data); 
+  if (response) { 
+     
+  } 
+  showKdrama(data); 
+} 
+
+function showKdrama(data){
 var i = 0;
 let drama = ``;
 let jdrama = ``;
@@ -97,61 +88,57 @@ break;
 }
 
 }
-if (! data2==null) {
-	for (let r of data2.drama) { 
-i ++;
-jdrama += `
-<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
-			<img class="serithumb" src="${r.drama_cover}">
-			<label class="title">${r.drama_title}</label>
-		</div>
 
-`;
-if (i == 6 ) {
-i = 0;	
-break; 	
-}
-
-}
-}
-if (! data3==null) {
-	for (let r of data3.drama) { 
-i ++;
-cdrama += `
-<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
-			<img class="serithumb" src="${r.drama_cover}">
-			<label class="title">${r.drama_title}</label>
-		</div>
-
-`;
-if (i == 6 ) {
-i = 0;	
-break; 	
-}
-
-}
-}
-if (! data4==null) {
-	for (let r of data4.drama) { 
-i ++;
-anime += `
-<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
-			<img class="serithumb" src="${r.drama_cover}">
-			<label class="title">${r.drama_title}</label>
-		</div>
-
-`;
-if (i == 6 ) {
-i = 0;	
-break; 	
-}
-
-}
-}
+	
 
 document.getElementById("K-Drama").innerHTML = drama;
-document.getElementById("J-Drama").innerHTML = jdrama;
-document.getElementById("Chinese-Drama").innerHTML = cdrama; 
-document.getElementById("hai-Drama").innerHTML = anime;
+
 
 }
+const Jdrama = "https://creator.n1channel.org/drama/search.php/?s=japanesedrama";
+getapij(Jdrama);
+async function getapij(url) { 
+  
+  // Storing response 
+  const response = await fetch(url); 
+  
+  
+  // Storing data in form of JSON 
+  var data = await response.json();
+  
+  console.log(data); 
+  if (response) { 
+     
+  } 
+  showJdrama(data); 
+} 
+
+function showJdrama(data){
+var i = 0;
+let drama = ``;
+
+
+
+for (let r of data.drama) { 
+i ++;
+drama += `
+<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
+			<img class="serithumb" src="${r.drama_cover}">
+			<label class="title">${r.drama_title}</label>
+		</div>
+
+`;
+if (i == 6 ) {
+i = 0;	
+break; 	
+}
+
+}
+
+	
+
+document.getElementById("J-Drama").innerHTML = drama;
+
+
+}
+
