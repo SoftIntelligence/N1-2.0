b844bf65f8caa6c76a1c4f5cc3fbe46d9a277fb2
root <root@localhost.localdomain>
2020-12-14 16:05:58 +0630

    N1

--- themes/n1/static/js/home.js
+++ themes/n1/static/js/home.js
@@ -17,28 +17,38 @@ function showcat(){
 	
 }
 
-const kdrama = "https://creator.n1channel.org/drama/search.php/?s=KDrama";
+const Kdrama = "https://creator.n1channel.org/drama/search.php/?s=KDrama";
+const Jdrama = "https://creator.n1channel.org/drama/search.php/?s=JapaneseDrama";
+const ChineseDrama = "https://creator.n1channel.org/drama/search.php/?s=ChineseDrama";
+const Anime = "https://creator.n1channel.org/drama/search.php/?s=Anime";
+
 const ad = "https://creator.n1channel.org/ad/read.php";
 
 
 
-getapi(kdrama);
+getapi(Kdrama,Jdrama,ChineseDrama,Anime);
 getapiad(ad); 
 
 
 
-async function getapi(url) { 
+async function getapi(url,url2,url3,url4) { 
   
   // Storing response 
   const response = await fetch(url); 
+  const response2 = await fetch(url2);
+  const response3= await fetch(url3);
+  const response4 = await fetch(url4);   
   
   // Storing data in form of JSON 
-  var data = await response.json(); 
+  var data = await response.json();
+  var data2 = await response2.json(); 
+  var data3 = await response3.json();
+  var data4 = await response4.json();
   console.log(data); 
   if (response) { 
      
   } 
-  showKdrama(data); 
+  showKdrama(data,data2.data3,data4); 
 } 
 async function getapiad(url) { 
   
@@ -64,9 +74,13 @@ document.getElementById("hpad2").src = r.url;
 }	
 }	
 }
-function showKdrama(data){
+function showKdrama(data,data2,data3,data4){
 var i = 0;
 let drama = ``;
+let jdrama = ``;
+let cdrama = ``;
+let anime = ``;
+
 
 for (let r of data.drama) { 
 i ++;
@@ -78,11 +92,66 @@ drama += `
 
 `;
 if (i == 6 ) {
+i = 0;	
 break; 	
 }
 
 }
+if (! data2==null) {
+	for (let r of data2.drama) { 
+i ++;
+jdrama += `
+<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
+			<img class="serithumb" src="${r.drama_cover}">
+			<label class="title">${r.drama_title}</label>
+		</div>
 
-document.getElementById("K-Drama").innerHTML = drama; 
+`;
+if (i == 6 ) {
+i = 0;	
+break; 	
+}
+
+}
+}
+if (! data3==null) {
+	for (let r of data3.drama) { 
+i ++;
+cdrama += `
+<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
+			<img class="serithumb" src="${r.drama_cover}">
+			<label class="title">${r.drama_title}</label>
+		</div>
+
+`;
+if (i == 6 ) {
+i = 0;	
+break; 	
+}
+
+}
+}
+if (! data4==null) {
+	for (let r of data4.drama) { 
+i ++;
+anime += `
+<div class="serie" onclick="location.href='/kdrama/?id=${r.drama_id}'">
+			<img class="serithumb" src="${r.drama_cover}">
+			<label class="title">${r.drama_title}</label>
+		</div>
+
+`;
+if (i == 6 ) {
+i = 0;	
+break; 	
+}
+
+}
+}
+
+document.getElementById("K-Drama").innerHTML = drama;
+document.getElementById("J-Drama").innerHTML = jdrama;
+document.getElementById("Chinese-Drama").innerHTML = cdrama; 
+document.getElementById("hai-Drama").innerHTML = anime;
 
 }
